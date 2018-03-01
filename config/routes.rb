909fefcdb79d6bb6e1d91cb886ea313150e34d09
notebookk <tian.gan2017@gmail.com>
2018-03-01 18:22:03 -0500

    use ruby way to point to other page(s)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
 
-
   get 'contactUs' => 'pages#contactUs'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
